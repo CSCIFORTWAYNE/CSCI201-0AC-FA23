1419f9e1f90040100a808dac8e7021b1315113ae
Angela <avenable11@ivytech.edu>
2023-08-21 16:01:06 -0400

    function demo 8/21/23

--- 08_21_2023/main.cpp
+++ 08_21_2023/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <string>
+
+int getInt(std::string);
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    int i;
+    i = getInt("Enter the number of apples you would like.");
+    std::cout << "You got " << i << " apples." << std::endl;
     return 0;
+}
+
+int getInt(std::string prompt)
+{
+    int input;
+    std::cout << prompt << std::endl;
+    std::cin >> input;
+    std::cout << input << std::endl;
+    while (!std::cin)
+    {
+        if (!std::cin)
+        {
+            std::cout << "You need to enter a number." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+        }
+        std::cout << prompt << std::endl;
+        std::cin >> input;
+        std::cout << input << std::endl;
+    }
+    return input;
 }
