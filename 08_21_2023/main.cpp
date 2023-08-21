828017576853a2f09bcb05362cbc96517afe0775
Angela <avenable11@ivytech.edu>
2023-08-21 16:08:23 -0400

    added a simple struct 8/21/23

--- 08_21_2023/main.cpp
+++ 08_21_2023/main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <string>
 
+struct order
+{
+    int apples;
+    int peaches;
+    int pears;
+};
+
 int getInt(std::string);
 
 int main()
 {
     int i;
+    order myOrder;
+    order yourOrder;
+    order allOrders[10];
     i = getInt("Enter the number of apples you would like.");
     std::cout << "You got " << i << " apples." << std::endl;
+    myOrder.apples = i;
+    allOrders[0].apples = i;
     return 0;
 }
 
