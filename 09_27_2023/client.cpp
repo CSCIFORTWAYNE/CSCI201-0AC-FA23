4c326264d280717afc8c8333df9fbbbe3250a373
avenable11 <avenable11@ivytech.edu>
2023-09-27 14:40:08 -0400

    final demo with strings 9/27/2023

--- 09_27_2023/client.cpp
+++ 09_27_2023/client.cpp
@@ -1,49 +1,40 @@
 #include "PracticalSocket.h"
 #include <iostream>
-#include <limits>
-
-void resetStream();
+#include <string>
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        std::cerr << "Usage: ./client <server host>" << std::endl;
+        std::cerr << "Usage: ./client <server host address>" << std::endl;
         return 1;
     }
 
     try
     {
-        std::cout << argv[1] << std::endl;
+        std::string message;
+        std::cout << "Enter a message for the server: ";
+        std::getline(std::cin >> std::ws, message);
+        std::cout << std::endl;
         TCPSocket sock(argv[1], 9431);
-        int input;
-        std::cout << "Enter the starting number: ";
-        std::cin >> input;
-        while (!std::cin)
-        {
-            resetStream();
-            std::cout << "Enter the starting number: ";
-            std::cin >> input;
-        }
-        uint32_t val = static_cast<uint32_t>(input);
-        val = ntohl(val);
+        uint32_t val = htonl(message.length());
         sock.send(&val, sizeof(val));
+        sock.send(message.c_str(), message.length());
         if (sock.recvFully(&val, sizeof(val)) == sizeof(val))
         {
             val = ntohl(val);
-            std::cout << "Server Response: " << val << std::endl;
+            char *buffer = new char[val + 1];
+            if (sock.recvFully(buffer, val) == val)
+            {
+                buffer[val] = '\0';
+                std::cout << "Server Response: " << buffer << std::endl;
+            }
+            delete[] buffer;
         }
     }
     catch (SocketException e)
     {
         std::cerr << e.what() << '\n';
     }
-
     return 0;
-}
-
-void resetStream()
-{
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
--- 09_27_2023/server.cpp
+++ 09_27_2023/server.cpp
@@ -1,5 +1,10 @@
 #include "PracticalSocket.h"
 #include <iostream>
+#include <string>
+#include <map>
+#include <algorithm>
+
+std::string processString(std::string);
 
 int main(int argc, char *argv[])
 {
@@ -9,13 +14,25 @@ int main(int argc, char *argv[])
         for (;;)
         {
             TCPSocket *sock = servSock.accept();
-            u_int32_t val;
+            std::cout << "	\033[1m\033[38;5;2m" << std::endl;
+            std::cout << "Handling Client" << std::endl;
+            uint32_t val;
             if (sock->recvFully(&val, sizeof(val)) == sizeof(val))
             {
                 val = ntohl(val);
-                val++;
-                val = htonl(val);
-                sock->send(&val, sizeof(val));
+                std::cout << "receiving: " << val << " bytes" << std::endl;
+                char *buffer = new char[val + 1];
+                if (sock->recvFully(buffer, val) == val)
+                {
+                    buffer[val] = '\0';
+                    std::string input(buffer);
+                    std::string response = processString(input);
+                    std::cout << "received: " << input << std::endl;
+                    val = htonl(response.length());
+                    sock->send(&val, sizeof(val));
+                    sock->send(response.c_str(), response.length());
+                }
+                delete[] buffer;
             }
             delete sock;
         }
@@ -24,6 +41,22 @@ int main(int argc, char *argv[])
     {
         std::cerr << e.what() << '\n';
     }
-
-    return 0;
+}
+// lecture activity add 3 incoming strings and responses to the map
+std::string processString(std::string inStr)
+{
+    static std::map<std::string, std::string> strMap = {{"hello world", "Goodbye"},
+                                                        {"how are you?", "OK"},
+                                                        {"is the weather nice today?", "No"},
+                                                        {"goodbye", "Have a nice day!"}};
+    std::transform(inStr.begin(), inStr.end(), inStr.begin(), ::tolower);
+    if (strMap.count(inStr))
+    {
+        return strMap[inStr];
+    }
+    else
+    {
+        strMap[inStr] = "I've seen this before.";
+        return inStr;
+    }
 }
